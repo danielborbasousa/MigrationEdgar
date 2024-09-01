4878f4f72a84b74db26d3b9231a2311a90782f17
danielborbasousa <tsox555@gmail.com>
2024-09-01 10:22:58 -0300

    Primeiro commit - adicionando todos os arquivos

---
+++ database/migrations/2024_09_01_124010_create_employees.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('restaurant_id');
+            $table->string('name');
+            $table->string('position');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->decimal('salary', 8, 2);
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
